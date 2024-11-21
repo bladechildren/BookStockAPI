22e3d620076c657a1f7006a2a357fb79c19fcd0b
KKBANK\chatchawan.sara <chatchawan.sara@kkpfg.com>
2024-11-21 12:08:15 +0700

    initial

---
+++ src/test/kotlin/com/teaparty/BookStockAPI/BookStockApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teaparty.BookStockAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookStockApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
